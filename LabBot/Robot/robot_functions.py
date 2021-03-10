0338568ef47dc2911be57ef9e307bc3392085653
seppi40 <lukas.maechler@rm-optik.ch>
2021-03-10 09:22:16 +0100

    Circle finshed Transition finished

--- LabBot/Robot/robot_functions.py
+++ LabBot/Robot/robot_functions.py
@@ -2,12 +2,54 @@ import math
 
 def circle(positionRobot_x, positionRobot_y, angleRobot):
     angleTarget = getAngle(positionRobot_x, positionRobot_y, positionTarget_x, positionTarget_y)
-    if circle == 1:
+    point1_x =
+    point1_y =
+    point2_x =
+    point2_y =.
+    point3_x =
+    point3_y =
+    point4_x =
+    point4_y =
+    point5_x =
+    point5_y =
+    point6_x =
+    point6_y =
+    if circle == 0:
+        finished = point(point1_x, point1_y,positionRobot[0],positionRobot[1],positionRobot[2])
+        if finished == 1:
+            circle = 1
+            finished = 0
+    elif circle == 1:
+        finished = point(point2_x, point2_y,positionRobot[0],positionRobot[1],positionRobot[2])
+        if finished == 1:
+            circle = 2
+            finished = 0
+    elif circle == 2:
+        finished = point(point3_x, point3_y,positionRobot[0],positionRobot[1],positionRobot[2])
+        if finished == 1:
+            circle = 3
+            finished = 0
+    elif circle == 3:
+        finished = point(point4_x, point4_y,positionRobot[0],positionRobot[1],positionRobot[2])
+        if finished == 1:
+            circle = 4
+            finished = 0
+    elif circle == 4:
+        finished = point(point5_x, point5_y,positionRobot[0],positionRobot[1],positionRobot[2])
+        if finished == 1:
+            circle = 5
+            finished = 0
+    elif circle == 5:
+        finished = point(point6_x, point6_y,positionRobot[0],positionRobot[1],positionRobot[2])
+        if finished == 1:
+            circle = 0
+            finished = 0
+    
         
     #circle Function
     
     
-def angleTarget(xRobot, yRobot, xTarget, yTarget):
+def getAngle(xRobot, yRobot, xTarget, yTarget):
     if (xTarget-xRobot)== 0 and (yTarget-yRobot)<=0:
         angle = 270
     elif (xTarget-xRobot)== 0 and (yTarget-yRobot)>=0:
@@ -37,23 +79,30 @@ def point(positionTarget_x, positionTarget_y, positionRobot_x, positionRobot_y, 
     
     angleTarget = getAngle(positionRobot_x, positionRobot_y, positionTarget_x, positionTarget_y)
     
-        
+    
     
     if angleTarget - 5 > angleRobot:
-        rightMotor(1)
+        ultSensor = dataUlt()
+        if ultSensor != 1:
+            rightMotor(1)
+        finished = 0
         #angleTarget = getAngle(positionRobot_x, positionRobot_y, positionTarget_x, positionTarget_y)
-    if angleTarget + 5 < angleRobot:
-        leftMotor(1)
+    elif angleTarget + 5 < angleRobot:
+        ultSensor = dataUlt()
+        if ultSensor != 1:
+            leftMotor(1)
+        finished = 0
         #angleTarget = getAngle(positionRobot_x, positionRobot_y, positionTarget_x, positionTarget_y)
-    if (positionRobot_x < positionTarget_x - 5 or positionRobot_x > positionTarget_x + 5) and (positionRobot_y < positionTarget_y - 5 or positionRobot_y > positionTarget_y + 5):
+    elif (positionRobot_x < positionTarget_x - 5 or positionRobot_x > positionTarget_x + 5) and (positionRobot_y < positionTarget_y - 5 or positionRobot_y > positionTarget_y + 5):
         ultSensor = dataUlt()
         if ultSensor != 1:
             leftMotor(1)
             rightMotor(1)
+        finished = 0
         #angleTarget = getAngle(positionRobot_x, positionRobot_y, positionTarget_x, positionTarget_y)
     else:
         finished = 1
-        return finished
+    return finished
     
     
     
--- LabBot/Robot/robot_main.py
+++ LabBot/Robot/robot_main.py
@@ -1,7 +1,8 @@
 from functions import *
 from circle import*
+from time import sleep
 
-
+transition = 0
 while True:
     mode = bluetooth[5]
     used = bluetooth[6]
@@ -15,15 +16,45 @@ while True:
     
     middleRight = 300
     middleLeft = 200
-    
-    
+    pointRightMiddle_x =
+    pointRightMiddle_y =
+    pointLeftMiddle_x =
+    pointLeftMiddle_y =
     if mode == 3:
         if used == 1:
-            if positionRobot[0] > middleRight:
-                point(pointRightMiddle
-            elif positionTarget[0] < middleLeft:
-                
-            point(positionTarget[0],positionTarget[1],positionRobot[0],positionRobot[1],positionRobot[2])
+            if positionRobot[0] > middleLeft and positionTarget[0] < middleLeft:
+                transitionNeed = 1
+            elif positionRobot[0] < middleRight and positionTarget[0] > middleRight:
+                transitionNeed = 2
+            if transitionNeed == 1
+                if transition == 0:
+                    finished = point(pointRightMiddle_x, pointRightMiddle_y,positionRobot[0],positionRobot[1],positionRobot[2])
+                    if finished == 1:
+                        transition = 1
+                        finished = 0
+                if transition == 1:
+                    finished = point(pointLeftMiddle_x, pointLeftMiddle_y,positionRobot[0],positionRobot[1],positionRobot[2])
+                    if finished == 1:
+                        transitionNeed = 0
+                        finished = 0
+                        transition = 0
+            elif:
+                if transition == 0:
+                    finished = point(pointLeftMiddle_x, pointLeftMiddle_y,positionRobot[0],positionRobot[1],positionRobot[2])
+                    if finished == 1:
+                        transition = 1
+                        finished = 0
+                if transition == 1:
+                    finished = point(pointRightMiddle_x, pointRightMiddle_y,positionRobot[0],positionRobot[1],positionRobot[2])
+                    if finished == 1:
+                        transitionNeed = 0
+                        finished = 0
+                        transition = 0
+            else:
+                finished = point(positionTarget[0], positionTarget[1],positionRobot[0],positionRobot[1],positionRobot[2])
+                if finished == 1:
+                    sleep(5)
+                    
             
         
         else:
@@ -32,7 +63,6 @@ while True:
     elif mode == 2:
         
         circle()
-        
     
     elif mode == 1:
         
